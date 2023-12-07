f5c11b83a833781606b8abeec42754d048923c62
bugsnotabunny <malakhov.malakhit@gmail.com>
2023-12-07 09:18:27 +0300

    statically sized vector

---
+++ inc/vector.hpp
@@ -0,0 +1,462 @@
+#ifndef STATIC_VECTOR_HPP
+#define STATIC_VECTOR_HPP
+
+#include <algorithm>
+#include <bits/iterator_concepts.h>
+#include <bits/ranges_base.h>
+#include <cassert>
+#include <cstddef>
+#include <initializer_list>
+#include <iterator>
+#include <memory>
+#include <new>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+namespace static_containers
+{
+    template < typename T, size_t N >
+    class Vector
+    {
+       public:
+        using ValueType = T;
+        using SizeType = size_t;
+        using DifferenceType = std::ptrdiff_t;
+        using Reference = T &;
+        using ConstReference = const T &;
+        using Pointer = T *;
+        using ConstPointer = const T *;
+        using Iterator = T *;
+        using ConstIterator = const T *;
+        using ReverseIterator = std::reverse_iterator< T * >;
+        using ConstReverseIterator = std::reverse_iterator< T * >;
+
+        constexpr Vector() noexcept:
+          size_(0)
+        {}
+
+        constexpr Vector(std::initializer_list< T > init):
+          Vector()
+        {
+            assign(init);
+        }
+
+        constexpr Vector(SizeType count, const T & value):
+          Vector()
+        {
+            assign(count, value);
+        }
+
+        constexpr Vector(SizeType count):
+          Vector()
+        {
+            assign(count, T{});
+        }
+
+        template < std::input_iterator It >
+        constexpr Vector(It begin, It end):
+          Vector()
+        {
+            assign(begin, end);
+        }
+
+        constexpr Vector(const Vector & rhs):
+          size_(0)
+        {
+            for (const auto & item: rhs)
+            {
+                push_back(item);
+            }
+        }
+
+        constexpr Vector(Vector && rhs):
+          size_(rhs.size_)
+        {
+            assign(rhs.begin(), rhs.end());
+            rhs.size_ = 0;
+        }
+
+        constexpr Vector & operator=(std::initializer_list< T > init)
+        {
+            Vector tmp{ std::move(init) };
+            std::swap(*this, tmp);
+            return *this;
+        }
+
+        constexpr Vector & operator=(const Vector & rhs)
+        {
+            Vector tmp{ rhs };
+            std::swap(*this, tmp);
+            return *this;
+        }
+
+        constexpr Vector & operator=(Vector && rhs)
+        {
+            std::swap(*this, rhs);
+            return *this;
+        }
+
+        ~Vector()
+        {
+            std::destroy(begin(), end());
+        }
+
+        constexpr void assign(SizeType count, const T & value)
+        {
+            while (size_ < count)
+            {
+                push_back(value);
+            }
+        }
+
+        template < std::input_iterator It >
+        constexpr void assign(It begin, It end)
+        {
+            for (; begin != end; ++begin)
+            {
+                push_back(std::move(*begin));
+            }
+        }
+
+        constexpr void assign(std::initializer_list< T > init)
+        {
+            for (auto && elem: init)
+            {
+                push_back(std::move(elem));
+            }
+        }
+
+        constexpr Reference at(SizeType pos)
+        {
+            const auto & self = *this;
+            return const_cast< Reference >(self.at(pos));
+        }
+
+        constexpr ConstReference at(SizeType pos) const
+        {
+            if (pos >= size_)
+            {
+                throw std::out_of_range{ "static vector index is out of range" };
+            }
+            return operator[](pos);
+        }
+
+        constexpr Reference operator[](SizeType pos) noexcept
+        {
+            const auto & self = *this;
+            return const_cast< Reference >(self[pos]);
+        }
+
+        constexpr ConstReference operator[](SizeType pos) const noexcept
+        {
+            assert(pos < size_);
+            return *(cbegin() + pos);
+        }
+
+        constexpr Reference front() noexcept
+        {
+            const auto & self = *this;
+            return const_cast< Reference >(self.front());
+        }
+
+        constexpr ConstReference front() const noexcept
+        {
+            assert(!empty());
+            return operator[](0);
+        }
+
+        constexpr Reference back() noexcept
+        {
+            const auto & self = *this;
+            return const_cast< Reference >(self.back());
+        }
+
+        constexpr ConstReference back() const noexcept
+        {
+            assert(!empty());
+            return operator[](size_ - 1);
+        }
+
+        constexpr Pointer data() noexcept
+        {
+            const auto & self = *this;
+            return const_cast< Pointer >(self.data());
+        }
+
+        constexpr ConstPointer data() const noexcept
+        {
+            return cbegin();
+        }
+
+        constexpr Iterator begin() noexcept
+        {
+            return const_cast< Iterator >(cbegin());
+        }
+
+        constexpr ConstIterator begin() const noexcept
+        {
+            return cbegin();
+        }
+
+        constexpr ConstIterator cbegin() const noexcept
+        {
+            return std::launder(reinterpret_cast< const T * >(std::addressof(storage_[0])));
+        }
+
+        constexpr Iterator end() noexcept
+        {
+            return const_cast< Iterator >(cend());
+        }
+
+        constexpr ConstIterator end() const noexcept
+        {
+            return cend();
+        }
+
+        constexpr ConstIterator cend() const noexcept
+        {
+            return cbegin() + size_;
+        }
+
+        constexpr ReverseIterator rbegin() noexcept
+        {
+            return end();
+        }
+
+        constexpr ConstReverseIterator rbegin() const noexcept
+        {
+            return end();
+        }
+
+        constexpr ConstReverseIterator crbegin() const noexcept
+        {
+            return cend();
+        }
+
+        constexpr ReverseIterator rend() noexcept
+        {
+            return begin();
+        }
+
+        constexpr ConstReverseIterator rend() const noexcept
+        {
+            return begin();
+        }
+
+        constexpr ConstReverseIterator crend() const noexcept
+        {
+            return cbegin();
+        }
+
+        [[nodiscard]] constexpr bool empty() const noexcept
+        {
+            return size_ == 0;
+        }
+
+        constexpr SizeType size() const noexcept
+        {
+            return size_;
+        }
+
+        static constexpr SizeType capacity() noexcept
+        {
+            return max_size();
+        }
+
+        static constexpr SizeType max_size() noexcept
+        {
+            return N;
+        }
+
+        template < typename... Args >
+        constexpr Reference emplace_back(Args &&... args)
+        {
+            return *emplace(cend(), std::forward< Args >(args)...);
+        }
+
+        constexpr void push_back(const T & value)
+        {
+            emplace_back(value);
+        }
+
+        constexpr void push_back(T && value)
+        {
+            emplace_back(std::move(value));
+        }
+
+        constexpr void pop_back()
+        {
+            assert(!empty());
+            std::destroy_at(std::addressof(operator[](size_ - 1)));
+            --size_;
+        }
+
+        constexpr void clear() noexcept
+        {
+            std::destroy(begin(), end());
+            size_ = 0;
+        }
+
+        constexpr void resize(SizeType count)
+        {
+            resize(count, ValueType{});
+        }
+
+        constexpr void resize(SizeType count, const ValueType & value)
+        {
+            if (size_ == count)
+            {
+                return;
+            }
+            else if (size_ > count)
+            {
+                std::destroy(begin() + count, end());
+                size_ = count;
+                return;
+            }
+            while (size_ < count)
+            {
+                emplace_back(value);
+            }
+        }
+
+        constexpr Iterator insert(ConstIterator pos, const T & value)
+        {
+            T to_insert{ value };
+            return insert(pos, std::move(to_insert));
+        }
+
+        constexpr Iterator insert(ConstIterator pos, T && value) noexcept
+        {
+            return emplace(pos, std::move(value));
+        }
+
+        constexpr Iterator insert(ConstIterator pos, SizeType count, const T & value) noexcept
+        {
+            std::vector< T > to_insert(count, value);
+            return insert(pos, to_insert.begin(), to_insert.end());
+        }
+
+        template < std::input_iterator It >
+        constexpr Iterator insert(ConstIterator pos, It first, It last) noexcept
+        {
+            SizeType amount_to_insert = std::distance(first, last);
+
+            assert(size_ + amount_to_insert <= capacity());
+
+            shift_elems(amount_to_insert, end() - 1, std::distance(pos, cend()));
+            auto pos_idx = pos - begin();
+            for (SizeType i = pos_idx; i != pos_idx + amount_to_insert; ++first, ++i)
+            {
+                construct_at(i, std::move(*first));
+            }
+            size_ += amount_to_insert;
+            return begin() + pos_idx;
+        }
+
+        constexpr Iterator insert(ConstIterator pos, std::initializer_list< T > init) noexcept
+        {
+            return insert(pos, init.begin(), init.end());
+        }
+
+        template < typename... Args >
+        constexpr Iterator emplace(ConstIterator pos, Args &&... args)
+        {
+            assert(size_ + 1 <= capacity());
+            shift_elems(1, end() - 1, std::distance(pos, cend()));
+            auto pos_idx = pos - begin();
+            construct_at(pos_idx, std::forward< Args >(args)...);
+            ++size_;
+            return begin() + pos_idx;
+        }
+
+        constexpr Iterator erase(ConstIterator pos)
+        {
+            auto pos_mut = begin() + (pos - begin());
+            std::destroy_at(pos_mut);
+            shift_elems(-1, pos_mut + 1, std::distance(pos_mut, end()));
+            --size_;
+            return pos_mut;
+        }
+
+        constexpr Iterator erase(ConstIterator first, ConstIterator last)
+        {
+            auto first_mut = begin() + (first - begin());
+            auto last_mut = begin() + (last - begin());
+            auto count = std::distance(first, last);
+            std::destroy(first_mut, last_mut);
+            shift_elems(-count, last_mut, std::distance(first_mut, end()));
+            size_ -= count;
+            return first_mut;
+        }
+
+       private:
+        template < typename... Args >
+        constexpr void construct_at(SizeType pos, Args... args)
+        {
+            assert(pos < N);
+            ::new (std::addressof(storage_[pos])) T(std::forward< Args >(args)...);
+        }
+
+        constexpr static void shift_elems(DifferenceType shift, Iterator from, SizeType count)
+        {
+            auto sign = [](auto num)
+            {
+                return (num > 0) - (num < 0);
+            };
+            DifferenceType inc = -sign(shift);
+            Iterator to = from + shift;
+            for (SizeType i = 0; i < count; from += inc, to += inc, ++i)
+            {
+                *to = std::move(*from);
+            }
+        }
+
+        SizeType size_;
+        std::aligned_storage_t< sizeof(T), alignof(T) > storage_[N];
+    };
+
+    template < typename T, size_t N >
+    constexpr bool operator==(const Vector< T, N > & lhs, const Vector< T, N > & rhs)
+    {
+        bool result = lhs.size() == rhs.size();
+        auto lit = lhs.begin();
+        auto rit = rhs.begin();
+        for (; result && lit != lhs.end(); ++lit, ++rit)
+        {
+            result = result && *lit == *rit;
+        }
+        return result;
+    }
+
+    template < typename T, size_t N >
+    constexpr auto operator<=>(const Vector< T, N > & lhs, const Vector< T, N > & rhs)
+    {
+        return std::lexicographical_compare_three_way(lhs.begin(),
+         lhs.end(),
+         rhs.begin(),
+         rhs.end());
+    }
+
+    template < typename T, size_t N, typename U >
+    constexpr typename Vector< T, N >::SizeType erase(Vector< T, N > & c, const U & value)
+    {
+        auto it = std::remove(c.begin(), c.end(), value);
+        auto count = c.end() - it;
+        c.erase(it, c.end());
+        return count;
+    }
+
+    template < typename T, size_t N, typename Pred >
+    constexpr typename Vector< T, N >::SizeType erase_if(Vector< T, N > & c, Pred pred)
+    {
+        auto it = std::remove_if(c.begin(), c.end(), pred);
+        auto r = c.end() - it;
+        c.erase(it, c.end());
+        return r;
+    }
+}
+
+#endif
---
+++ tests/vector.cpp
@@ -0,0 +1,279 @@
+#include <algorithm>
+#include <cassert>
+#include <catch2/catch_test_macros.hpp>
+#include <cstddef>
+#include <functional>
+#include <ranges>
+#include <vector>
+
+#include "vector.hpp"
+
+namespace stct = static_containers;
+
+namespace
+{
+    const stct::Vector< int, 3 > TEST_DEFAULT{ 0, 1, 2 };
+
+    class DropLogger
+    {
+       public:
+        DropLogger(size_t & cnt):
+          cnt_(cnt)
+        {}
+
+        DropLogger(const DropLogger &) = default;
+
+        DropLogger(DropLogger && rhs):
+          cnt_(rhs.cnt_),
+          moved_(rhs.moved_),
+          destroyed_(rhs.destroyed_)
+        {
+            rhs.moved_ = true;
+        }
+
+        DropLogger & operator=(const DropLogger &) = default;
+
+        DropLogger & operator=(DropLogger && rhs)
+        {
+            std::swap(*this, rhs);
+            return *this;
+        }
+
+        ~DropLogger()
+        {
+            assert(!destroyed_);
+            destroyed_ = true;
+            if (!moved_)
+            {
+                ++cnt_;
+            }
+        }
+
+       private:
+        std::reference_wrapper< size_t > cnt_;
+        bool moved_ = false;
+        bool destroyed_ = false;
+    };
+}
+
+TEST_CASE("empty vector")
+{
+    stct::Vector< int, 2000 > vec;
+    REQUIRE(vec.empty());
+}
+
+TEST_CASE("initialize vector from count")
+{
+    stct::Vector< int, 3 > vec;
+    vec.assign(3, 23);
+    for (auto && elem: vec)
+    {
+        REQUIRE(elem == 23);
+    }
+}
+
+TEST_CASE("initialize vector from iters")
+{
+    std::vector< int > src = { 0, 1, 2 };
+    stct::Vector< int, 3 > dest;
+    dest.assign(src.begin(), src.end());
+    REQUIRE(dest[0] == 0);
+    REQUIRE(dest[1] == 1);
+    REQUIRE(dest[2] == 2);
+}
+
+TEST_CASE("initialize vector from init list")
+{
+    stct::Vector< int, 3 > vec;
+    vec.assign({ 0, 1, 2 });
+    REQUIRE(vec[0] == 0);
+    REQUIRE(vec[1] == 1);
+    REQUIRE(vec[2] == 2);
+}
+
+TEST_CASE("vector destruction")
+{
+    static size_t destructed = 0;
+    auto logger = DropLogger{ destructed };
+    {
+        stct::Vector< DropLogger, 10 > vec{ 10, logger };
+    }
+    REQUIRE(destructed == 10);
+    destructed = 0;
+}
+
+TEST_CASE("vector at")
+{
+    stct::Vector a(TEST_DEFAULT);
+    REQUIRE(a.at(2) == 2);
+    REQUIRE_THROWS(a.at(134134));
+}
+
+TEST_CASE("vector front")
+{
+    stct::Vector a(TEST_DEFAULT);
+    REQUIRE(a.back() == 2);
+    REQUIRE(a.front() == 0);
+
+    const stct::Vector b(TEST_DEFAULT);
+    REQUIRE(b.back() == 2);
+    REQUIRE(b.front() == 0);
+}
+
+TEST_CASE("vector iteration")
+{
+    stct::Vector a(TEST_DEFAULT);
+    int i = 0;
+    for (auto && elem: a)
+    {
+        REQUIRE(elem == i);
+        ++i;
+    }
+
+    const stct::Vector b(TEST_DEFAULT);
+    int j = 0;
+    for (auto && elem: b)
+    {
+        REQUIRE(elem == j);
+        ++j;
+    }
+}
+
+TEST_CASE("vector rev iteration")
+{
+    stct::Vector a(TEST_DEFAULT);
+    int i = 2;
+    for (auto && elem: std::views::reverse(a))
+    {
+        REQUIRE(elem == i);
+        --i;
+    }
+
+    const stct::Vector b(TEST_DEFAULT);
+    int j = 2;
+    for (auto && elem: std::views::reverse(b))
+    {
+        REQUIRE(elem == j);
+        --j;
+    }
+}
+
+TEST_CASE("vector pop_back")
+{
+    static size_t destructed = 0;
+    auto logger = DropLogger{ destructed };
+    stct::Vector< DropLogger, 3 > a(3, logger);
+    REQUIRE(a.size() == 3);
+    a.pop_back();
+    REQUIRE(a.size() == 2);
+    REQUIRE(destructed == 1);
+}
+
+TEST_CASE("vector clear")
+{
+    static size_t destructed = 0;
+    auto logger = DropLogger{ destructed };
+    stct::Vector< DropLogger, 3 > a(3, logger);
+    a.clear();
+    REQUIRE(a.empty());
+    REQUIRE(destructed == 3);
+}
+
+TEST_CASE("vector resize")
+{
+    SECTION("shrink")
+    {
+        static size_t destructed = 0;
+        auto logger = DropLogger{ destructed };
+        stct::Vector< DropLogger, 15 > a(13, logger);
+        a.resize(4, logger);
+        REQUIRE(destructed == 9);
+        REQUIRE(a.size() == 4);
+    }
+    SECTION("grow")
+    {
+        stct::Vector< int, 15 > a(3, 1);
+        a.resize(12, 2);
+        REQUIRE(a.size() == 12);
+        for (auto it = a.begin() + 3; it != a.end(); ++it)
+        {
+            REQUIRE(*it == 2);
+        }
+    }
+}
+
+TEST_CASE("vector insert")
+{
+    using VecT = stct::Vector< int, 100 >;
+    VecT base(3, 1);
+    SECTION("one value")
+    {
+        VecT expected = { 1, 2, 1, 1 };
+        auto vec(base);
+        auto it = vec.insert(vec.begin() + 1, 2);
+        REQUIRE(*it == 2);
+        REQUIRE(vec == expected);
+    }
+    SECTION("many values")
+    {
+        VecT expected = { 1, 2, 2, 2, 1, 1 };
+        auto vec(base);
+        auto it = vec.insert(vec.begin() + 1, 3, 2);
+        REQUIRE(*it == 2);
+        REQUIRE(vec == expected);
+    }
+    SECTION("init list")
+    {
+        VecT expected = { 1, 3, 4, 5, 1, 1 };
+        auto vec(base);
+        auto it = vec.insert(vec.begin() + 1, { 3, 4, 5 });
+        REQUIRE(*it == 3);
+        REQUIRE(vec == expected);
+    }
+}
+
+TEST_CASE("vector erase")
+{
+    using VecT = stct::Vector< int, 100 >;
+    SECTION("one value")
+    {
+        VecT expected = { 1, 1, 3 };
+        VecT vec = { 1, 1, 2, 3 };
+        auto it = vec.erase(vec.begin() + 2);
+        REQUIRE(*it == 3);
+        REQUIRE(vec == expected);
+    }
+    SECTION("iterator range")
+    {
+        VecT expected = { 1, 3 };
+        VecT vec = { 1, 2, 2, 2, 3 };
+        auto it = vec.erase(vec.begin() + 1, vec.end() - 1);
+        REQUIRE(*it == 3);
+        REQUIRE(vec == expected);
+    }
+    SECTION("erase non-trival type")
+    {
+        auto vec = stct::Vector< std::string, 20 >{ "dsaad", "aaaa", "auidisfou" };
+        vec.erase(vec.begin());
+    }
+    SECTION("remove-erase")
+    {
+        VecT expected = { 4, 3 };
+        VecT vec = { 4, 2, 2, 2, 3 };
+        auto removed = stct::erase(vec, 2);
+        REQUIRE(removed == 3);
+        REQUIRE(vec == expected);
+    }
+    SECTION("remove-erase if")
+    {
+        VecT expected = { 3 };
+        VecT vec = { 4, 2, 2, 2, 3 };
+        auto removed = stct::erase_if(vec,
+         [](const auto & elem)
+         {
+             return elem % 2 == 0;
+         });
+        REQUIRE(removed == 4);
+        REQUIRE(vec == expected);
+    }
+}
