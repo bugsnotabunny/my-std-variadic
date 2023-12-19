5752c262cdc5ce4f4e39cd113fee1ddc24b8c0fc
bugsnotabunny <malakhov.malakhit@gmail.com>
2023-12-19 08:13:53 +0300

    const methods

--- inc/map.hpp
+++ inc/map.hpp
@@ -26,6 +26,35 @@ namespace static_containers
                 x(rhs, lhs)
             } -> std::convertible_to< bool >;
         };
+
+        template < bool isconst, size_t I = 0, typename F, typename... Values >
+        constexpr auto visit_at_impl(size_t idx,
+         F f,
+         std::conditional_t< isconst, const Tuple< Values... > &, Tuple< Values... > & > values)
+        {
+            if constexpr (I < sizeof...(Values) - 1)
+            {
+                switch (idx)
+                {
+                case I:
+                    return f(get< I >(values));
+                default:
+                    return visit_at_impl< isconst, I + 1, F, Values... >(idx,
+                     std::forward< F >(f),
+                     values);
+                }
+            }
+            else if constexpr (I == sizeof...(Values) - 1)
+            {
+                switch (idx)
+                {
+                case I:
+                    return f(get< I >(values));
+                default:
+                    assert(false);
+                }
+            }
+        }
     }
 
     template < typename Key,
@@ -34,26 +63,37 @@ namespace static_containers
     class Map
     {
        public:
-        constexpr Map(std::array< Key, sizeof...(Values) > keys,
+        constexpr Map(std::array< Key, sizeof...(Values) > sortedkeys,
          Tuple< Values... > values,
-         Comparator cmp):
-          keys_(std::move(keys)),
+         Comparator cmp) noexcept:
+          keys_(std::move(sortedkeys)),
           values_(std::move(values))
         {
             std::ranges::sort(keys_, std::forward< Comparator >(cmp));
+            assert(std::ranges::is_sorted(sortedkeys));
         }
 
         template < typename F >
         constexpr auto visit_at(const Key & key, F f)
         {
-            return visit_at_impl(idx(key), std::forward< F >(f));
+            return detail::visit_at_impl< false, 0, F, Values... >(idx(key),
+             std::forward< F >(f),
+             values_);
         }
 
-        constexpr size_t idx(const Key & key)
+        template < typename F >
+        constexpr auto visit_at(const Key & key, F f) const
+        {
+            return detail::visit_at_impl< true, 0, F, Values... >(idx(key),
+             std::forward< F >(f),
+             values_);
+        }
+
+        constexpr size_t idx(const Key & key) const
         {
             auto it = std::ranges::upper_bound(keys_, key);
             --it;
-            Key * k = *(it) == key ? std::addressof(*it) : nullptr;
+            const Key * k = *(it) == key ? std::addressof(*it) : nullptr;
             assert(k != nullptr);
             size_t idx = k - keys_.begin();
             return idx;
@@ -65,28 +105,13 @@ namespace static_containers
             return get< I >(values_);
         }
 
-       private:
-        template < size_t I = 0, typename F, typename... Args >
-        auto visit_at_impl(size_t idx, F f)
+        template < size_t I >
+        constexpr const auto & at() const
         {
-            if constexpr (I < decltype(values_)::size())
-            {
-                switch (idx)
-                {
-                case I:
-                    return f(get< I >(values_));
-                default:
-                    return visit_at_impl< I + 1, F >(idx, std::forward< F >(f));
-                }
-            }
-            else
-            {
-                // a trick to outwit return type deduction
-                assert(false);
-                return visit_at_impl< I - 1, F >(idx, std::forward< F >(f));
-            }
+            return get< I >(values_);
         }
 
+       private:
         std::array< Key, sizeof...(Values) > keys_;
         Tuple< Values... > values_;
     };
@@ -94,29 +119,39 @@ namespace static_containers
     template < typename Key,
      detail::Comparator< Key > Comparator = std::less< Key >,
      typename... Values >
-    auto make_map(Tuple< Key, Values >... keyvals)
+    constexpr auto make_map(Comparator cmp, Tuple< Key, Values >... keyvals) noexcept
     {
-        return make_map(Comparator{}, std::move(keyvals)...);
+        return Map{ std::array{ std::forward< Key >(get< 0 >(keyvals))... },
+            Tuple{ std::forward< Values >(get< 1 >(keyvals))... },
+            std::forward< Comparator >(cmp) };
     }
 
     template < typename Key,
      detail::Comparator< Key > Comparator = std::less< Key >,
      typename... Values >
-    auto make_map(Comparator cmp, Tuple< Key, Values >... keyvals)
+    constexpr auto make_map(Tuple< Key, Values >... keyvals) noexcept
     {
-        std::array< Key, sizeof...(Values) > keys{ std::forward< Key >(get< 0 >(keyvals))... };
-        Tuple< Values... > values{ std::forward< Values >(get< 1 >(keyvals))... };
-        return Map{ std::move(keys), std::move(values), std::forward< Comparator >(cmp) };
+        return make_map(Comparator{}, std::forward< Tuple< Key, Values > >(keyvals)...);
     }
 }
 
 namespace static_containers::visitors
 {
     template < typename Key, typename... Args >
-    constexpr auto call_at(auto & map, const Key & key, Args &&... args)
+    constexpr auto call_at(auto & map, const Key & key, Args... args)
     {
         return map.visit_at(key,
          [&](auto & f)
+         {
+             return f(std::forward< Args >(args)...);
+         });
+    }
+
+    template < typename Key, typename... Args >
+    constexpr auto call_at(const auto & map, const Key & key, Args... args)
+    {
+        return map.visit_at(key,
+         [&](const auto & f)
          {
              return f(std::forward< Args >(args)...);
          });
@@ -141,6 +176,7 @@ namespace static_containers::visitors
              --i;
          });
     }
+
 }
 
 #endif
