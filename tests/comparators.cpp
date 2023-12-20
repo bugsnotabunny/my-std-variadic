67e3930f36495e19245c554dc32e0f4f40bcd596
bugsnotabunny <malakhov.malakhit@gmail.com>
2023-12-20 10:56:39 +0300

    alignof comparison

--- tests/comparators.cpp
+++ tests/comparators.cpp
@@ -28,25 +28,25 @@ TEST_CASE("sizeof cmp")
     REQUIRE(!cmp::size_of::greatereq_v< char, int >);
 }
 
-TEST_CASE("sizeof cmp")
+TEST_CASE("alignof cmp")
 {
-    REQUIRE(cmp::size_of::eq_v< char, char >);
-    REQUIRE(!cmp::size_of::eq_v< char, int >);
+    REQUIRE(cmp::align_of ::eq_v< char, char >);
+    REQUIRE(!cmp::align_of::eq_v< char, int >);
 
-    REQUIRE(cmp::size_of::neq_v< char, int >);
-    REQUIRE(!cmp::size_of::neq_v< char, char >);
+    REQUIRE(cmp::align_of::neq_v< char, int >);
+    REQUIRE(!cmp::align_of::neq_v< char, char >);
 
-    REQUIRE(cmp::size_of::grearer_v< int, char >);
-    REQUIRE(!cmp::size_of::grearer_v< char, int >);
+    REQUIRE(cmp::align_of::grearer_v< int, char >);
+    REQUIRE(!cmp::align_of::grearer_v< char, int >);
 
-    REQUIRE(cmp::size_of::less_v< char, int >);
-    REQUIRE(!cmp::size_of::less_v< int, char >);
+    REQUIRE(cmp::align_of::less_v< char, int >);
+    REQUIRE(!cmp::align_of::less_v< int, char >);
 
-    REQUIRE(cmp::size_of::lesseq_v< char, int >);
-    REQUIRE(cmp::size_of::lesseq_v< char, char >);
-    REQUIRE(!cmp::size_of::lesseq_v< int, char >);
+    REQUIRE(cmp::align_of::lesseq_v< char, int >);
+    REQUIRE(cmp::align_of::lesseq_v< char, char >);
+    REQUIRE(!cmp::align_of::lesseq_v< int, char >);
 
-    REQUIRE(cmp::size_of::greatereq_v< int, char >);
-    REQUIRE(cmp::size_of::greatereq_v< char, char >);
-    REQUIRE(!cmp::size_of::greatereq_v< char, int >);
+    REQUIRE(cmp::align_of::greatereq_v< int, char >);
+    REQUIRE(cmp::align_of::greatereq_v< char, char >);
+    REQUIRE(!cmp::align_of::greatereq_v< char, int >);
 }
